5e3b4986ed61f24e6c3cdb1c5e9a6d3017f54a92
Shjani49 <shjani49@gmail.com>
2020-08-17 13:17:52 -0600

    Enqueue the theme stylesheet.

--- techcareers/functions.php
+++ techcareers/functions.php
@@ -9,8 +9,14 @@ add_action( 'wp_enqueue_scripts', function() {
         'techcareers-theme-scripts', // Our new name for this script file.
         get_theme_file_uri( '/assets/js/scripts.js' ), // URL / path to your script.
         //array( 'react', 'jquery' ), // Does your script depend on other Js libraries /files?
-        strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ), // Version number of this script.
+        strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ), // Version number of this script. prevent from browser crash
         FALSE // Output in the "footer"? <Before </body> closing tag.)
     );
-
+    // Get our CSS ready to output in the <head> via wp_head() (see header.php)
+    wp_enqueue_style(
+        'techcareers-theme-styles', // Our new name for this stylesheet file.
+        get_theme_file_uri( '/assets/css/main.css' ),// URL / path to your styles.
+        strftime( get_theme_file_uri( '/assets/css/main.css' ) ), // version number of your stylesheet
+        'all' // What sort of stylesheet (media query) is this?
+    );
 } );
