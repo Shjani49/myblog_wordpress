c83225beda8a4f6b16931f066462d77614cb5ba8
Shjani49 <shjani49@gmail.com>
2020-08-17 11:32:56 -0600

    Add Publish Date and Categories links to single.php.

--- techcareers/page.php
+++ techcareers/page.php
@@ -1,5 +1,8 @@
 <?php get_header();?>
 
-<p> This is page.php</p>
+<article>
+<h2>Page: <?php the_title(); //Output this page's title. ?></h2>
+<?php the_content(); //Output this page's content. ?>
+</article>
 
 <?php get_footer();?>
--- techcareers/single.php
+++ techcareers/single.php
@@ -1,5 +1,35 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<p> This is single.php</p>
+<article>
+    <h2>Hello World: <?php the_title(); //Output this page's title. 
+                        ?></h2>
+    <p>
+        This page post was published on:
+        <time datetime="<?php echo get_the_date(DATE_ATOM); // THE BLOG POST PUBLISHED DATE FROM WP. 
+                        ?>">
+            <?php echo get_the_date(); // The blog post publish date from wp. 
+            ?>
+        </time>
+    </p>
+    <h3>Categories:</h3>
+    <ul>
+        <?php
 
-<?php get_footer();?>
+        // Get the categories for this post. (just an array of their category ID numbers.)
+        $post_categories = wp_get_post_categories(get_the_id()); // get_the_id() gets the current post/page ID.
+        // Loop through the categories for this post.
+        foreach ($post_categories as $c) {
+            // Get the category object (WP_Term class instance.)
+            $cat = get_category($c); // Returns a WP_Term object.
+            // Get the URL for this category so we can link to it.
+            $catURL = get_term_link($cat);
+            // Echo each category as a list item.
+            echo "<li><a href=\"{$catURL}\">{$cat->name}</a></li>";
+        }
+        ?>
+    </ul>
+    <?php the_content(); //Output this page's content. 
+    ?>
+</article>
+
+<?php get_footer(); ?>
